7d4c19a3783caf5957f051f0d9d6e70293c26cde
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-21 17:10:32 -0500

    asdf

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,48 @@
 public class HelloWorld {
 
     public static void main (String[] args){
-//        in java strings are only defined with double quotes "this is strings"
-        // character is defined  by single quote 'x'
-        System.out.println("Hello, World!");
-//        sout = shorthand for System.out.println();
+    ////        in java strings are only defined with double quotes "this is strings"
+    //        // character is defined  by single quote 'x'
+    //        System.out.println("Hello, World!");
+    ////        sout = shorthand for System.out.println();
+    //        int myFavoriteNumber = 10;
+    //        System.out.println(myFavoriteNumber);
+    //        String myString = "Hey, this is my string";
+    //        int myString2 = 1;
+    //        myString += myString2;
+    //        System.out.println(myString);
+    //        float myNumber = 3.14f;
+    //        System.out.println(myNumber);
+    //        int x = 5;
+    //        System.out.println(x++);
+    //        System.out.println(x);
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+//        var class = 1
 
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+////        int three = (int) o;
+//        System.out.println(theNumberThree);
+//        System.out.println(o);
+//        int three = (int) "three";
+//        System.out.println(three);
+
+//        int x = 4;
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x /= y;
+//        y -= x;
+
+
+        byte myByte = 128;
+        System.out.println(myByte);
     }
 }
