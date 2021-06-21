cebf81044b540d590d406a48b3323ca8aadae881
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-21 15:17:29 -0500

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+    public static void main (String[] args){
+//        in java strings are only defined with double quotes "this is strings"
+        // character is defined  by 'x'
+        System.out.println("Hello, World!");
+//        sout = shorthand for System.out.println();
+
+    }
+}
