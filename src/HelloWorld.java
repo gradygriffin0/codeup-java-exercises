d018fe819b9f5adc1100648913e5d621b906dd4b
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-22 09:42:47 -0500

    2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,10 +4,11 @@ public class HelloWorld {
     ////        in java strings are only defined with double quotes "this is strings"
     //        // character is defined  by single quote 'x'
     //        System.out.println("Hello, World!");
-    ////        sout = shorthand for System.out.println();
-    //        int myFavoriteNumber = 10;
+        ////        sout = shorthand for System.out.println();
 
+        //        int myFavoriteNumber = 10;
     //        System.out.println(myFavoriteNumber);
+
     //        String myString = "Hey, this is my string";
     //        int myString2 = 1;
     //        myString += myString2;
