4105120d705c74a8786c34efd9133c999b3a93d9
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-22 11:38:10 -0500

    3

--- src/ConsoleIOLecture.java
+++ src/ConsoleIOLecture.java
@@ -21,18 +21,39 @@ public class ConsoleIOLecture {
 //        int currencyInPennies = 1000;
 //        System.out.printf("I'll sell you my swamp for $%.2f per acre", currencyInPennies / 100.00);
 
-//        create new scanner object with name scnr
-//        new instance
-//        the argument () is where you want scanner to look or observe
-        Scanner scnr = new Scanner(System.in);
-//        prompt user a question
-        System.out.println("What is your name? First and Last");
-//        declare a var for the next thing input
-//        scanner will watch for it
-        String firstName = scnr.next();
-        String lastName = scnr.next();
+// Scanner
+    //        create new scanner object with name scnr
+    //        new instance
+    //        the argument () is where you want scanner to look or observe
+            Scanner scnr = new Scanner(System.in);
+    //        prompt user a question
+    //        System.out.println("What is your name? First and Last");
+    //        declare a var for the next thing input
+    //        scanner will watch for it
+    //        String firstName = scnr.next();
+    //        String lastName = scnr.next();
 
-//        log scanner
-        System.out.printf("Your name is %s %s.", firstName, lastName);
+    //        log scanner
+    //        System.out.printf("Your name is %s %s. \n", firstName, lastName);
+    //        String sentence = String.format("Your first name is: %s \nYour last name is: %s\n", firstName, lastName);
+    //        System.out.println(sentence);
+
+//        nextInt
+//        System.out.println("Enter a number: ");
+//        int myNum = scnr.nextInt();
+//        System.out.println(myNum);
+
+//        nextLine()
+//        System.out.println("enter a line:");
+//        String myString = scnr.nextLine();
+//        System.out.println(myString);
+
+        while (scnr.hasNext()){
+            String word = scnr.next();
+            System.out.print(word + " ");
+            if (word.equals("break")){
+                break;
+            }
+        }
     }
 }
