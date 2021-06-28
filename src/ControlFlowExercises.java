b5ad0bb30c1a94ef99b79c343c4533f85476c722
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-28 11:56:37 -0500

    exercise 3

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,61 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises  {
     public static void main(String[] args) {
 
-    }
+
+//        #1
+//    int i = 5;
+//    while((i <= 15)){
+//        System.out.print(i + " ");
+//        i++;
+//    }
+
+//        2
+//        int count = 0;
+//        do {
+//            count += 2;
+//            System.out.println(count);
+//        }while (count < 100);
+//    }
+
+//    int count = 100;
+//        do {
+//        count -= 5;
+//        System.out.println(count);
+//    }while (count > -10);
+//        int count = 2;
+//        do {
+//            count = (int) Math.pow((count), 2);
+//            System.out.println(count);
+//        } while(count < 65536);
+//        for(int i = 0; i < 100; i++){
+//            System.out.println(i);
+//            if(((i % 3 == 0) && (i % 5 == 0))){
+//                System.out.println("FIZZBUZZ");
+//            }
+//            else if (i % 3 == 0){
+//                System.out.println("fizz");
+//            }
+//            else if (i % 5 == 0){
+//
+//                System.out.println("buzz");
+//            }
+
+//        3
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter a starting integer");
+//        int input = sc.nextInt();
+//        System.out.println("What integer would you like to go up to?");
+//        int max = sc.nextInt();
+//        System.out.println("Number | Squared | Cubed ");
+//        System.out.println("--------------------------");
+//        for(int i = input; i <= max; i++){
+//            System.out.println(i + "    |     " + (int) Math.pow(i, 2) + "    |     " + (int) Math.pow(i, 3));
+//
+//
+//
+//
+//        }
+}
 }
--- src/StringsLecture.java
+++ src/StringsLecture.java
@@ -11,25 +11,40 @@ public class StringsLecture {
 //            System.out.println("Hey that's my name!");
 
 
+//        Scanner sc = new Scanner(System.in);
+//        String[] studentsArr =
+//                {"Amado", "Christian", "Christopher", "Corey", "Demetrio",
+//                        "Diamond", "Erik", "Grady", "Jesse", "Joshua",
+//                        "Juan", "Mitchell", "Moses", "Nathan", "Prachi",
+//                        "Raul", "Ricardo", "Richard", "Robert", "Sam",
+//                        "Sean", "Stephen", "Tiffanny", "Wagner", "Laura",
+//                        "Casey", "Dorian", "Tristan", "Jordy"};
+//
+//        System.out.println("Enter your name");
+//        int randomNum = (int) (Math.random() * 28);
+//        String randomName = studentsArr[randomNum];
+//        String input = sc.next();
+//        while (!input.equalsIgnoreCase(randomName)){
+//
+//            randomNum = (int) (Math.random() * 28);
+//            randomName = studentsArr[randomNum];
+//            System.out.printf("%s \n", randomName);
+//
+//        }
+        //        TODO: create a String variable that holds this sentence: "Red
+//         Orange Blue Yellow Green Purple". Prompt the user to type in
+//         their favorite color, if their fave color is the same as the
+//         first word in the String, then print out "my fave color:
+//         userColor is the first one". If their fave color is last, then print out "my fave color: userColor is last"
+        String sentence = "Red Orange Blue Yellow Green Purple";
         Scanner sc = new Scanner(System.in);
-        String[] studentsArr =
-                {"Amado", "Christian", "Christopher", "Corey", "Demetrio",
-                        "Diamond", "Erik", "Grady", "Jesse", "Joshua",
-                        "Juan", "Mitchell", "Moses", "Nathan", "Prachi",
-                        "Raul", "Ricardo", "Richard", "Robert", "Sam",
-                        "Sean", "Stephen", "Tiffanny", "Wagner", "Laura",
-                        "Casey", "Dorian", "Tristan", "Jordy"};
-
-        System.out.println("Enter your name");
-        int randomNum = (int) (Math.random() * 28);
-        String randomName = studentsArr[randomNum];
+        System.out.println("enter a color");
         String input = sc.next();
-        while (!input.equalsIgnoreCase(randomName)){
-
-            randomNum = (int) (Math.random() * 28);
-            randomName = studentsArr[randomNum];
-            System.out.printf("%s \n", randomName);
-
+        if (sentence.toUpperCase().startsWith(input.toUpperCase())){
+            System.out.println("your favorite color is the start of sentence");
+        }
+        if (sentence.toUpperCase().endsWith(input.toUpperCase())){
+            System.out.println("your favorite color is the end of sentence");
         }
 
 
