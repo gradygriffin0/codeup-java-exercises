3c739b36293c9ea9bcc4350a24954401c39bb3bd
gradygriffin0 <grady.griffin0@gmail.com>
2021-07-01 16:58:33 -0500

    oop

--- src/Person.java
+++ src/Person.java
@@ -2,6 +2,10 @@ public class Person {
     public static void main(String[] args) {
         Person me = new Person("Grady");
         System.out.println(me.sayHello());
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
     }
 
     private String name;
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,4 +1,35 @@
 package util;
 
+import java.util.Scanner;
+
+
 public class Input {
+    public static void main(String[] args) {
+        Input test = new Input();
+        System.out.println(test.getString());
+        System.out.println(test.yesNo());
+    }
+
+    private Scanner scanner;
+
+    public Input() {
+        this.scanner = new Scanner(System.in);
+
+    }
+
+    public String getString() {
+        System.out.println("enter a string: ");
+        return scanner.nextLine();
+    }
+
+    public boolean yesNo() {
+        System.out.println("Is it yes or not yes?: ");
+        boolean yes = false;
+        String value = scanner.next();
+        if ((value.equalsIgnoreCase("yes")) || (value.equalsIgnoreCase("y"))|| (value.equalsIgnoreCase("yeah")) || (value.equalsIgnoreCase("yea"))) {
+            yes = true;
+        }
+        return (yes);
+    }
+
 }
