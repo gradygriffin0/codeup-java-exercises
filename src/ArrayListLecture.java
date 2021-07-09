0f3084cafe8f8ecde2fb8311ea5e1c8b13d272ea
gradygriffin0 <grady.griffin0@gmail.com>
2021-07-09 10:55:03 -0500

    arraylist

---
+++ src/ArrayListLecture.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class ArrayListLecture {
+    public static void main(String[] args) {
+        ArrayList<String> stringArr = new ArrayList<>();
+        stringArr.add("Casey");
+        System.out.println(stringArr);
+    }
+}
