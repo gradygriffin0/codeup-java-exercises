b53c640a7f726846aab199df82ba23d61e5f2453
gradygriffin0 <grady.griffin0@gmail.com>
2021-07-16 09:31:46 -0500

    failed warmup :(

---
+++ src/warmup/PrimeNumbers.java
@@ -0,0 +1,24 @@
+package warmup;
+
+import java.util.ArrayList;
+
+public class PrimeNumbers {
+    public static void main(String[] args) {
+        System.out.println(primeNumbers());
+    }
+    public static ArrayList<Integer> primeNumbers(){
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 20; i++){
+            for (int j = 1; j <= 20; j++) {
+                System.out.println(j);
+                System.out.println(i);
+                if (i % j == 0){
+                    System.out.println(i + " is not a prime number");
+                    numbers.add(i);
+                    continue;
+                }
+            }
+        }
+        return numbers;
+    }
+}
